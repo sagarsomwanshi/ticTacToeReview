c77ad000df3cbae28bc844d74b1024c36fdd35d6
sagarsomwanshi <sagarsomwanshi13@gmail.com>
2023-02-07 09:17:28 +0530

    Gives user option of choosing Play letter

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,8 +1,10 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class TicTacToeGame {
     public static void main(String[] args) {
         UC1();
+        UC2();
 
     }
     public static void UC1(){
@@ -11,5 +13,18 @@ public class TicTacToeGame {
             board[i] = String.valueOf(i);
         }
         System.out.println(Arrays.toString(board));
+    }
+    public static void UC2(){
+        System.out.println("Please choose your letter for playing (X or O) \nType letter & press Enter ");
+        Scanner sc = new Scanner(System.in);
+        String playLetter = sc.next();
+        if(playLetter.equals("x")){
+            System.out.println("you chose X\nNow Computer will play with O");
+        } else if (playLetter.equals("o")) {
+            System.out.println("you chose O\nNow Computer will play with X");
+        }else {
+            System.out.println("Invalid Input");
+            UC2();
+        }
     }
 }
